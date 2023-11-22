27b5b3d8e06c475dbf8a2838034dff5a897357a2
Alvaro Orlando <alvaroorlando@AlvaroBook-Pro.local>
2023-11-22 19:43:59 -0300

    feat(components): add Heading component to storybook

---
+++ packages/react/src/components/Heading.tsx
@@ -0,0 +1,28 @@
+import { styled } from '../styles/index'
+import { ElementType, ComponentProps } from 'react'
+
+export const Heading = styled('h2', {
+  fontFamily: '$default',
+  lineHeight: '$shorter',
+  margin: 0,
+  color: '$gray100',
+
+  variants: {
+    size: {
+      sm: { fontSize: 'xl' },
+      md: { fontSize: '2xl' },
+      lg: { fontSize: '4xl' },
+      '2xl': { fontSize: '6xl' },
+      '3xl': { fontSize: '7xl' },
+      '4xl': { fontSize: '8xl' },
+      '5xl': { fontSize: '9xl' },
+    },
+  },
+  defaultVariants: {
+    size: 'md',
+  },
+})
+
+export interface HeadingProps extends ComponentProps<typeof Heading> {
+  as?: ElementType
+}
