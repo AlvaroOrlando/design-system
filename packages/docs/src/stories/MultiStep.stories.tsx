cb112e23d5b3ec3162789b30d96c123317a04cc1
Alvaro Orlando <alvaroorlando@AlvaroBook-Pro.local>
2023-11-24 01:14:26 -0300

    chore: configure displayName for Storybook components

---
+++ packages/docs/src/stories/MultiStep.stories.tsx
@@ -0,0 +1,32 @@
+import { StoryObj, Meta } from '@storybook/react'
+import { Box, Text, MultiStep, MultiStepProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Form/MultiStep',
+  tags: ['autodocs'],
+  component: MultiStep,
+  args: {
+    size: 4,
+    currentStep: 1,
+  },
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+        >
+          {Story()}
+        </Box>
+      )
+    },
+  ],
+} as Meta<MultiStepProps>
+
+export const Primary: StoryObj<MultiStepProps> = {}
+
+export const Full: StoryObj<MultiStepProps> = {
+  args: {
+    currentStep: 4,
+  },
+}
