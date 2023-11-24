e81f19a7b1fd86a0ac89262f714a755ed63fe41c
Alvaro Orlando <alvarocondeorlando@gmail.com>
2023-11-24 16:48:53 -0300

    chore(storybook): update components for improved UI consistency

--- packages/docs/src/stories/TextInput.stories.tsx
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -30,6 +30,7 @@ export const Primary: StoryObj<TextInputProps> = {
 export const WithPrefix: StoryObj<TextInputProps> = {
   args: {
     prefix: 'cal.com/',
+    placeholder: 'your-username',
   },
 }
 
